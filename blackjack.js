691d0f8ee64a67a7e443c6648c26fd748b60e2ba
bluefoxbandido <adamthewonderboy@gmail.com>
2019-11-06 19:49:42 -0600

    Reboot fresh idea/first commit

---
+++ blackjack.js
@@ -0,0 +1,32 @@
+//Build deck
+const deck = () => {
+    const suits = ["Heart", "Spade", "Club", "Diamond"];
+    const cardValues = {
+        "Ace" : 11,
+        "Two" : 2,
+        "Three" : 3,
+        "Four" : 4,
+        "Five" : 5,
+        "Six" : 6,
+        "Seven" : 7,
+        "Eight" : 8, 
+        "Nine" : 9,
+        "Jack" : 10,
+        "Queen" : 10,
+        "King" : 10
+    }
+}
+
+//Give User 100
+//Player places a wager
+//Deal two cards to the dealer and to the dealer
+//show players hand and show dealers top card
+//if player card total is 21 they win
+//player can hit
+//if player card total is higher than 21, they lost
+//player can pass
+//if player card total is higher than dealer card total player wins
+//if player card total is equal to the dealer, player push
+//if player card total is less than the dealer, dealer wins
+//
+var response = prompt();
